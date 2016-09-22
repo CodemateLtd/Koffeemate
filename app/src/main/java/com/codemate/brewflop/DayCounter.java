eb233e5fc8d6ab22e48e2883010f42872d3faf07
Iiro Krankka <iiro.krankka@gmail.com>
2016-09-22 21:12:57 +0300

    Now sending an event whenever the day count is incremented or cleared.

--- app/src/main/java/com/codemate/brewflop/DayCounter.java
+++ app/src/main/java/com/codemate/brewflop/DayCounter.java
@@ -1,7 +1,9 @@
 package com.codemate.brewflop;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.content.LocalBroadcastManager;
 
 /**
  * Created by iiro on 22.9.2016.
@@ -9,11 +11,16 @@ import android.content.SharedPreferences;
 public class DayCounter {
     private static final String PREFS_NAME = "day_counter_prefs";
     private static final String KEY_DAY_COUNT = "day_count";
+    private static final String ACTION_DAY_COUNT_UPDATED = "com.codemate.brewflop.ACTION_DAY_COUNT_UPDATED";
 
     private final SharedPreferences preferences;
+    private final Intent dayCountUpdatedEvent;
+    private final LocalBroadcastManager broadcastManager;
 
     public DayCounter(Context context) {
         this.preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        this.dayCountUpdatedEvent = new Intent(ACTION_DAY_COUNT_UPDATED);
+        this.broadcastManager = LocalBroadcastManager.getInstance(context);
     }
 
     public void increment() {
@@ -22,6 +29,12 @@ public class DayCounter {
         preferences.edit()
                 .putInt(KEY_DAY_COUNT, newDayCount)
                 .apply();
+
+        sendDayCountUpdatedEvent();
+    }
+
+    private void sendDayCountUpdatedEvent() {
+        broadcastManager.sendBroadcast(dayCountUpdatedEvent);
     }
 
     public int getDayCount() {
@@ -30,5 +43,7 @@ public class DayCounter {
 
     public void clear() {
         preferences.edit().clear().apply();
+
+        sendDayCountUpdatedEvent();
     }
 }
