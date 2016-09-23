1f75f1ae616d98ff644dc20adea14f5eeb299c3d
Iiro Krankka <iiro.krankka@codemate.com>
2016-09-23 15:59:26 +0300

    Now the day count is updated in the UI.

--- app/src/main/java/com/codemate/brewflop/MainActivity.java
+++ app/src/main/java/com/codemate/brewflop/MainActivity.java
@@ -1,20 +1,78 @@
 package com.codemate.brewflop;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.codemate.brewflop.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private LocalBroadcastManager broadcastManager;
+    private DayCounter dayCounter;
+
+    private IntentFilter dayCountUpdatedFilter;
+    private BroadcastReceiver dayCountUpdatedReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        broadcastManager = LocalBroadcastManager.getInstance(this);
+        dayCounter = new DayCounter(this, broadcastManager);
 
         hideStatusBar();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        registerDayUpdateReceiver();
+        updateText();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        unregisterDayUpdateReceiver();
     }
 
     private void hideStatusBar() {
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+    }
+
+    private void updateText() {
+        int dayCount = dayCounter.getDayCount();
+        String formattedText = getString(R.string.days_fmt, dayCount);
+
+        binding.daysSinceLastIncident.setText(formattedText);
+    }
+
+    private void registerDayUpdateReceiver() {
+        if (dayCountUpdatedFilter == null || dayCountUpdatedReceiver == null) {
+            dayCountUpdatedFilter = new IntentFilter(DayCounter.ACTION_DAY_COUNT_UPDATED);
+            dayCountUpdatedReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    updateText();
+                }
+            };
+        }
+
+        broadcastManager.registerReceiver(dayCountUpdatedReceiver, dayCountUpdatedFilter);
+    }
+
+    private void unregisterDayUpdateReceiver() {
+        broadcastManager.unregisterReceiver(dayCountUpdatedReceiver);
     }
 }
