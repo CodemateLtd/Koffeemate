d04538f1c64c282c88eaa4a6022d368119213f5b
Iiro Krankka <iiro.krankka@codemate.com>
2016-10-10 12:56:03 +0300

    Added logic for not updating the incident-free days when it's weekend, because there can't be incidents on weekends since nobody works during them.

--- app/src/main/java/com/codemate/brewflop/alarms/AlarmReceiver.java
+++ app/src/main/java/com/codemate/brewflop/alarms/AlarmReceiver.java
@@ -6,14 +6,23 @@ import android.content.Intent;
 
 import com.codemate.brewflop.DayCounter;
 
+import java.util.Calendar;
+
 /**
  * Created by ironman on 22/09/16.
  */
 public class AlarmReceiver extends BroadcastReceiver {
     public DayCounter dayCounter;
+    public Calendar calendar = Calendar.getInstance();
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+
+        if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) {
+            return;
+        }
+
         if (dayCounter == null) {
             dayCounter = new DayCounter(context);
         }
--- app/src/test/java/com/codemate/brewflop/AlarmReceiverTest.java
+++ app/src/test/java/com/codemate/brewflop/AlarmReceiverTest.java
@@ -11,8 +11,13 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
+import java.util.Calendar;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by iiro on 22.9.2016.
@@ -22,20 +27,38 @@ public class AlarmReceiverTest {
     private Context context;
     private AlarmReceiver alarmReceiver;
     private DayCounter dayCounter;
+    private Calendar calendar;
 
     @Before
     public void setUp() {
         context = RuntimeEnvironment.application.getApplicationContext();
 
         dayCounter = mock(DayCounter.class);
+        calendar = mock(Calendar.class);
+
         alarmReceiver = new AlarmReceiver();
         alarmReceiver.dayCounter = dayCounter;
+        alarmReceiver.calendar = calendar;
     }
 
     @Test
-    public void testOnReceive() {
+    public void shouldUpdateDayCountBetweenMondayToFriday() {
+        for (int dayOfWeek = Calendar.MONDAY; dayOfWeek <= Calendar.FRIDAY; dayOfWeek++) {
+            when(calendar.get(Calendar.DAY_OF_WEEK)).thenReturn(dayOfWeek);
+            alarmReceiver.onReceive(context, new Intent());
+        }
+
+        verify(dayCounter, times(5)).increment();
+    }
+
+    @Test
+    public void shouldNotUpdateDayCountWhenSaturdayOrSunday() {
+        when(calendar.get(Calendar.DAY_OF_WEEK)).thenReturn(Calendar.SATURDAY);
         alarmReceiver.onReceive(context, new Intent());
 
-        verify(dayCounter).increment();
+        when(calendar.get(Calendar.DAY_OF_WEEK)).thenReturn(Calendar.SUNDAY);
+        alarmReceiver.onReceive(context, new Intent());
+
+        verifyZeroInteractions(dayCounter);
     }
 }
