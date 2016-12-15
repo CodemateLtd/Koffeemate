2158d1a507817b7ff9c111c83386e5d4460d90cc
Iiro Krankka <iiro.krankka@gmail.com>
2016-12-15 20:35:33 +0200

    Made a simple Trophy class that takes a number and sticks it on top of a trophy.

---
+++ app/src/main/java/com/codemate/brewflop/Trophy.kt
@@ -0,0 +1,27 @@
+package com.codemate.brewflop
+
+import android.content.Context
+import android.graphics.*
+
+object Trophy {
+    fun make(ctx: Context, value: Int): Bitmap {
+        val canvasBitmap = Bitmap.createBitmap(36, 36, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(canvasBitmap)
+
+        val trophy = BitmapFactory.decodeResource(ctx.resources, R.drawable.trophy)
+        val shadowPaint = Paint()
+
+        with(shadowPaint) {
+            color = Color.parseColor("#77000000")
+            textSize = 12f
+            typeface = Typeface.DEFAULT_BOLD
+            strokeWidth = 2.0f
+            style = Paint.Style.FILL
+        }
+
+        canvas.drawBitmap(trophy, 0f, 0f, null)
+        canvas.drawText(value.toString(), 14f, 15f, shadowPaint)
+
+        return canvasBitmap
+    }
+}
