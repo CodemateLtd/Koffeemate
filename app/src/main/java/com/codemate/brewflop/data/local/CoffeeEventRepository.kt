298e1cfb172b4a2f1e938ae19dfc76b922c75a0e
Iiro Krankka <iiro.krankka@gmail.com>
2016-12-18 11:42:20 +0200

    Added the functionality required to display the last brewing time in the UI.

--- app/src/main/java/com/codemate/brewflop/data/local/CoffeeEventRepository.kt
+++ app/src/main/java/com/codemate/brewflop/data/local/CoffeeEventRepository.kt
@@ -1,7 +1,10 @@
 package com.codemate.brewflop.data.local
 
+import com.codemate.brewflop.data.local.models.CoffeeBrewingEvent
+
 interface CoffeeEventRepository {
-    fun recordBrewingEvent()
-    fun recordBrewingAccident(userId: String)
+    fun recordBrewingEvent(): CoffeeBrewingEvent
+    fun recordBrewingAccident(userId: String): CoffeeBrewingEvent
     fun getAccidentCountForUser(userId: String): Long
+    fun getLastBrewingEvent(): CoffeeBrewingEvent?
 }
