2c5a42553f8364f33b9b18d1d40df77af1fe0f8c
Iiro Krankka <iiro.krankka@gmail.com>
2016-12-22 16:56:01 +0200

    Moved RealmCoffeeEventRepositoryTest to correct package.

--- app/src/androidTest/java/com/codemate/koffeemate/data/local/RealmCoffeeEventRepositoryTest.kt
+++ app/src/androidTest/java/com/codemate/koffeemate/data/local/RealmCoffeeEventRepositoryTest.kt
@@ -1,6 +1,21 @@
-package com.codemate.koffeemate.ui.main
+/*
+ * Copyright 2016 Codemate Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import com.codemate.koffeemate.data.local.RealmCoffeeEventRepository
+package com.codemate.koffeemate.data.local
+
 import com.codemate.koffeemate.data.local.models.CoffeeBrewingEvent
 import io.realm.Realm
 import io.realm.RealmConfiguration
