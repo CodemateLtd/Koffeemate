69a944a27db287712336e8bac5728377a98727d5
Iiro Krankka <iiro.krankka@gmail.com>
2016-12-22 19:51:56 +0200

    Reverted #16 as it actually made the app crash every time it was resumed.

--- app/src/main/java/com/codemate/koffeemate/data/local/RealmCoffeeEventRepository.kt
+++ app/src/main/java/com/codemate/koffeemate/data/local/RealmCoffeeEventRepository.kt
@@ -40,36 +40,26 @@ class RealmCoffeeEventRepository : CoffeeEventRepository {
         return event
     }
 
-    override fun getAccidentCountForUser(userId: String): Long {
-        val realm = Realm.getDefaultInstance()
-        val count = realm.where(CoffeeBrewingEvent::class.java)
+    override fun getAccidentCountForUser(userId: String) =
+            Realm.getDefaultInstance()
+                .where(CoffeeBrewingEvent::class.java)
                 .equalTo("isSuccessful", false)
                 .equalTo("userId", userId)
                 .count()
 
-        realm.close()
-        return count
-    }
-
     override fun getLastBrewingEvent(): CoffeeBrewingEvent? {
-        val realm = Realm.getDefaultInstance()
-        val event = realm.where(CoffeeBrewingEvent::class.java)
+        return Realm.getDefaultInstance()
+                .where(CoffeeBrewingEvent::class.java)
                 .equalTo("isSuccessful", true)
                 .findAllSorted("time", Sort.ASCENDING)
                 .lastOrNull()
-
-        realm.close()
-        return event
     }
 
     override fun getLastBrewingAccident(): CoffeeBrewingEvent? {
-        val realm = Realm.getDefaultInstance()
-        val accident = realm.where(CoffeeBrewingEvent::class.java)
+        return Realm.getDefaultInstance()
+                .where(CoffeeBrewingEvent::class.java)
                 .equalTo("isSuccessful", false)
                 .findAllSorted("time", Sort.ASCENDING)
                 .lastOrNull()
-
-        realm.close()
-        return accident
     }
 }
