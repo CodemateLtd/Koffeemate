d0b092dc1eb825ddaed09c6d796f72b3072d52b5
Iiro Krankka <iiro.krankka@gmail.com>
2016-12-22 19:36:28 +0200

    Cleaned up and removed the "Throws" annotations, since Exceptions on Kotlin are unchecked.

--- app/src/test/java/com/codemate/koffeemate/testutils/SynchronousExecutorService.kt
+++ app/src/test/java/com/codemate/koffeemate/testutils/SynchronousExecutorService.kt
@@ -16,36 +16,23 @@
 
 package com.codemate.koffeemate.testutils
 
-import java.util.concurrent.*
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
 
 class SynchronousExecutorService : ExecutorService {
-    override fun shutdown() {
-    }
-
+    override fun shutdown() { }
     override fun shutdownNow(): List<Runnable>? = null
     override fun isTerminated() = false
     override fun isShutdown() = false
-
-    @Throws(InterruptedException::class)
     override fun awaitTermination(timeout: Long, unit: TimeUnit) = false
-
     override fun <T> submit(task: Callable<T>): Future<T>? = null
     override fun <T> submit(task: Runnable, result: T): Future<T>? = null
     override fun submit(task: Runnable): Future<*>? = null
-
-    @Throws(InterruptedException::class)
     override fun <T> invokeAll(tasks: Collection<Callable<T>>): List<Future<T>>? = null
-
-    @Throws(InterruptedException::class)
     override fun <T> invokeAll(tasks: Collection<Callable<T>>, timeout: Long, unit: TimeUnit): List<Future<T>>? = null
-
-    @Throws(InterruptedException::class, ExecutionException::class)
     override fun <T> invokeAny(tasks: Collection<Callable<T>>): T? = null
-
-    @Throws(InterruptedException::class, ExecutionException::class, TimeoutException::class)
     override fun <T> invokeAny(tasks: Collection<Callable<T>>, timeout: Long, unit: TimeUnit): T? = null
-
-    override fun execute(command: Runnable) {
-        command.run()
-    }
+    override fun execute(command: Runnable) { command.run() }
 }
