6592e415f6742b195d10c3ec830ff0ad9265fabb
Iiro Krankka <iiro.krankka@gmail.com>
2016-12-26 16:40:50 +0200

    Added some touch feedback for the coffee pot view.

--- app/src/main/java/com/codemate/koffeemate/views/CoffeeProgressView.kt
+++ app/src/main/java/com/codemate/koffeemate/views/CoffeeProgressView.kt
@@ -2,13 +2,41 @@ package com.codemate.koffeemate.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.widget.FrameLayout
 import com.codemate.koffeemate.R
 import kotlinx.android.synthetic.main.view_coffee_progress.view.*
+import org.jetbrains.anko.onTouch
 
 class CoffeeProgressView(ctx: Context, attrs: AttributeSet) : FrameLayout(ctx, attrs) {
     init {
         inflate(ctx, R.layout.view_coffee_progress, this)
+        initializeTouchListener()
+    }
+
+    private fun initializeTouchListener() {
+        onTouch { view, motionEvent ->
+            when(motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> animateTouchDown()
+                MotionEvent.ACTION_UP -> animateTouchUp()
+            }
+
+            return@onTouch false
+        }
+    }
+
+    private fun animateTouchDown() {
+        animate().scaleX(0.9f)
+                .scaleY(0.9f)
+                .setDuration(100)
+                .start()
+    }
+
+    private fun animateTouchUp() {
+        animate().scaleX(1f)
+                .scaleY(1f)
+                .setDuration(100)
+                .start()
     }
 
     fun setProgress(newProgress: Int) {
