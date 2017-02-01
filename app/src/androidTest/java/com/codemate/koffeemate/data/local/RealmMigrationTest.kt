ed397a67a9654de845cae4b53518bf5f954504b8
Iiro Krankka <iiro.krankka@gmail.com>
2017-02-01 20:33:58 +0200

    Renamed "first", "second" and "third" variables to something more descriptive.

--- app/src/androidTest/java/com/codemate/koffeemate/data/local/RealmMigrationTest.kt
+++ app/src/androidTest/java/com/codemate/koffeemate/data/local/RealmMigrationTest.kt
@@ -58,29 +58,29 @@ class RealmMigrationTest {
         val all = realm.where(CoffeeBrewingEvent::class.java).findAll()
         assertThat(all.size, equalTo(3))
 
-        val first = all[0]
-        assertThat(first.id, equalTo("adf9c9b9-e521-462f-9d67-ff2a11d7b62c"))
-        assertThat(first.time, equalTo(1485872637115L))
-        assertThat(first.isSuccessful, equalTo(true))
-        assertThat(first.user, nullValue())
+        val brewingEventWithoutUserId = all[0]
+        assertThat(brewingEventWithoutUserId.id, equalTo("adf9c9b9-e521-462f-9d67-ff2a11d7b62c"))
+        assertThat(brewingEventWithoutUserId.time, equalTo(1485872637115L))
+        assertThat(brewingEventWithoutUserId.isSuccessful, equalTo(true))
+        assertThat(brewingEventWithoutUserId.user, nullValue())
 
-        val second = all[1]
-        assertThat(second.id, equalTo("0e742762-7181-4bc0-b7b5-d1ff68991dd6"))
-        assertThat(second.time, equalTo(1485872637117L))
-        assertThat(second.isSuccessful, equalTo(true))
-        assertThat(second.user!!.id, equalTo("abc-123"))
-        assertThat(second.user!!.last_updated, not(equalTo(0L)))
+        val brewingEventWithUserId = all[1]
+        assertThat(brewingEventWithUserId.id, equalTo("0e742762-7181-4bc0-b7b5-d1ff68991dd6"))
+        assertThat(brewingEventWithUserId.time, equalTo(1485872637117L))
+        assertThat(brewingEventWithUserId.isSuccessful, equalTo(true))
+        assertThat(brewingEventWithUserId.user!!.id, equalTo("abc-123"))
+        assertThat(brewingEventWithUserId.user!!.last_updated, not(equalTo(0L)))
 
-        val third = all[2]
-        assertThat(third.id, equalTo("480bb3b9-a01f-45cb-87cd-113465d4038a"))
-        assertThat(third.time, equalTo(1485872637118L))
-        assertThat(third.isSuccessful, equalTo(false))
-        assertThat(third.user!!.id, equalTo("abc-123"))
-        assertThat(second.user!!.last_updated, not(equalTo(0L)))
+        val brewingAccident = all[2]
+        assertThat(brewingAccident.id, equalTo("480bb3b9-a01f-45cb-87cd-113465d4038a"))
+        assertThat(brewingAccident.time, equalTo(1485872637118L))
+        assertThat(brewingAccident.isSuccessful, equalTo(false))
+        assertThat(brewingAccident.user!!.id, equalTo("abc-123"))
+        assertThat(brewingEventWithUserId.user!!.last_updated, not(equalTo(0L)))
 
         // Make sure we don't generate different timestamps for the users in
         // this new schema.
-        assertThat(second.user!!.last_updated, equalTo(third.user!!.last_updated))
+        assertThat(brewingEventWithUserId.user!!.last_updated, equalTo(brewingAccident.user!!.last_updated))
 
         realm.close()
     }
